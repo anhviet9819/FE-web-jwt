7475870c5fc4ab7690f7988d67c38ac451df0955
unknown <anhviet9819@gmail.com>
2020-12-24 22:22:21 +0700

    add DKK

--- src/screens/datlich/Dangkykham.js
+++ src/screens/datlich/Dangkykham.js
@@ -6,8 +6,11 @@ import Header from "../../components/Header";
 import hosoApi from "../../api/hosoApi";
 import AuthService from "../../services/auth.service";
 import bacsiApi from "../../api/bacsiApi";
+import "flatpickr/dist/flatpickr.css";
+import Flatpickr from "react-flatpickr";
 
 function Dangkykham() {
+  const [date, setDate] = useState(new Date());
   const [bacsiList, setBacsiList] = useState([]);
   useEffect(() => {
     const fetchProductList = async () => {
@@ -33,24 +36,56 @@ function Dangkykham() {
             </Link>
             {">"} ĐẶT KHÁM
           </div>
-          <div>
-            <p>Tên | Trình độ | Chuyên môn | Khoa | Cơ sở y tế | Tỉnh</p>
-            {bacsiList.map((elementDetail, index) => {
-              return (
-                <div>
-                  <div>
-                    {elementDetail.ten}|{elementDetail.trinhdo}|
-                    {elementDetail.chuyenkhoa}|{elementDetail.khoa.ten}|
-                    {elementDetail.khoa.cosoyte.ten}|
-                    {elementDetail.khoa.cosoyte.tinh.ten}
-                    <Link>
-                      Chọn ngày
-                    </Link>
-                  </div>
-                  <br />
-                </div>
-              );
-            })}
+          <div class="find-doctor-right">
+            <br />
+            <div class="user-navigate">
+              <h3> Vui lòng chọn bác sĩ</h3>
+            </div>
+            <div class="table-responsive find-doctor-right-cont table-hover">
+              <table class="table table-striped shadow-2">
+                <thead>
+                  <tr>
+                    {/* <th>#</th> */}
+                    <th>H.hàm,h.vị</th>
+                    <th>Họ và tên</th>
+                    <th>Chuyên môn</th>
+                    <th>Khoa</th>
+                    <th>Cơ sở y tế</th>
+                    <th>Tỉnh</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {bacsiList.map((elementDetail, index) => {
+                    return (
+                      <tr>
+                        <td>{elementDetail.ten}</td>
+                        <td>{elementDetail.trinhdo}</td>
+                        <td>{elementDetail.chuyenkhoa}</td>
+                        <td>{elementDetail.khoa.ten}</td>
+                        <td>{elementDetail.khoa.cosoyte.ten}</td>
+                        <td>{elementDetail.khoa.cosoyte.tinh.ten}</td>
+                        <td>
+                          <Flatpickr
+                            value={date} // giá trị ngày tháng
+                            // các option thêm cho thư viện
+                            options={{
+                              dateFormat: "d-m-Y H:m", // format ngày giờ
+                              enableTime: true
+                            }}
+                            // event
+                            onChange={(dateSelect) => setDate(dateSelect)}
+                          />{" "}
+                        </td>
+                        <Link className="btn btn-info" to="/xacnhandangky">
+                          Chọn ngày
+                        </Link>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
